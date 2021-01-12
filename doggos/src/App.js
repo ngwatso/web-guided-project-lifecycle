c75b299054780a95e4a7625cdd48463509bb8fa0
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-12 13:42:22 -0500

    lecture work-in-progress

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 class App extends React.Component {
     render() {
-        const dogs = [{name: "name1"}];
+        const dogs = [{name: "name1"}, {name: "name2"}];
         return(<div>
             <h1> Search the Dogs!</h1>
             <div className="dogContainer">
                 {
-                    dogs.map(dog=>{
-                        <h2>{dog.name}</h2>
-                    })
+                    dogs.map(dog=>(
+                    <h2>{dog.name}</h2>)
+                    )
                 }
             </div>
         </div>);
