b93ff144e544709fb3eccda1fd5fe070123be092
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-12 13:44:46 -0500

    lecture work-in-progress

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 class App extends React.Component {
+    state = {
+        dogImages: ["https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg"]
+    }
+
     render() {
-        const dogs = ["https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg", "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg"];
         return(<div>
             <h1> Search the Dogs!</h1>
             <div className="dogContainer">
                 {
-                    dogs.map(dogImage=>(
-                        <img src={dogImage}/>
+                    this.state.dogImages.map(image=>(
+                        <img src={image}/>
                     ))
                 }
             </div>
