8232a47b996e437fd726e75d176923ba96136914
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-12 13:03:36 -0500

    lecture work-in-progress

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -10,33 +10,7 @@ class App extends React.Component {
     pokemon: []
   }
 
-  componentDidMount() {
-    console.log("App: Component Mounted");
-    setTimeout(()=>{
-      this.setState({
-        pokemon: data
-      });
-    }, 3000);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log("App: Component updated");
-    
-    console.log("-----------");
-    console.log("old props: ", prevProps);
-    console.log("new props: ", this.props);
-
-    console.log("-----------");
-    console.log("old state: ", prevState);
-    console.log("new state: ", this.state);
-
-    if (this.state.pokemon.length > 0) {
-      console.log("New add to pokemon state data!!!");
-    }
-  }
-
   render() {
-    console.log("App: Component Rendered");
     return (
       <div className="App">
         {
